18bedd35cd84e0e39d8ac10893880cba098e8ad7
Joseph Gillen <joegillen@ymail.com>
2016-01-04 23:19:55 +0000

    Adding Unity Game Project

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+    private float moveSpeed;
+    // Use this for initialization
+    void Start () {
+        moveSpeed = 4.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+    }
+    void Movement()
+    {
+        // Jump right
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed);
+        }
+        // Jump left
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed);
+        }
+        // Jump forward
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformsMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformsMovement : MonoBehaviour {
+    private float counter, timeToMeet, moveSpeed;
+    private bool playerMovedForward;
+	// Use this for initialization
+	void Start () {
+        counter = 0; 
+        // The time the player has to move to the next platform.
+        timeToMeet = 100f;
+        // The boolean to check that the player is on the same
+        // platform row, otherwise the row moves forward
+        playerMovedForward = false;
+        // Setting the move speed
+        moveSpeed = 4.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlatforms();
+        counter++;
+        Debug.Log(counter);
+	}
+    void MovePlatforms()
+    {
+        if (counter >= timeToMeet && !playerMovedForward)
+        {
+            transform.Translate(Vector3.back * moveSpeed);
+            counter = 0;
+        }
+    }
+}
