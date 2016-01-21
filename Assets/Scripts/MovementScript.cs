d093ed7eeaa46f8f92cba0e06933b31ac7417284
Joseph Gillen <joegillen@ymail.com>
2016-01-21 21:56:05 +0000

    Adding new shit

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,32 +2,94 @@
 using System.Collections;
 
 public class MovementScript : MonoBehaviour {
-    private float moveSpeed;
+    private float moveSpeed, lerpTime, rateOfLerp;
+    private bool isLerping;
     // Use this for initialization
-    void Start () {
+    PlatformsMovement platMove = new PlatformsMovement();
+    // Lerping and coroutine variables
+    Vector3 newPosition;
+
+    void Start() {
+        // Grabbing the PlatformsMovement script from the Platforms game object
         moveSpeed = 4.0f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        platMove = GameObject.FindGameObjectWithTag("platforms").GetComponent<PlatformsMovement>();
+        // Initializing newPosition for use with movement
+        newPosition = new Vector3();
+        // Lerping variables
+        lerpTime = 0.0f;
+        rateOfLerp = 0.8f;
+        isLerping = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log("Player position: " + transform.position);
+    }
+    void FixedUpdate()
+    {
         Movement();
     }
     void Movement()
     {
-        // Jump right
-        if (Input.GetKeyUp(KeyCode.D))
+        // Jump right and forward
+        if (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.W) && !isLerping)
         {
-            transform.Translate(Vector3.right * moveSpeed);
+            isLerping = true;
+            newPosition = new Vector3(transform.position.x + 4, transform.position.y, transform.position.z + 4);
+            StartCoroutine(LerpTowards(newPosition, 3));
+        }
+        // Jump left and forward
+        else if (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.W) && !isLerping)
+        {
+            isLerping = true;
+            newPosition = new Vector3(transform.position.x - 4, transform.position.y, transform.position.z + 4);
+            StartCoroutine(LerpTowards(newPosition, 4));
+        }
+        // Jump right
+        else if (Input.GetKeyDown(KeyCode.D) && !isLerping)
+        {
+            isLerping = true;
+            newPosition = new Vector3(transform.position.x + 4, transform.position.y, transform.position.z);
+            StartCoroutine(LerpTowards(newPosition, 0));
         }
         // Jump left
-        else if (Input.GetKeyUp(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A) && !isLerping)
         {
-            transform.Translate(Vector3.left * moveSpeed);
+            isLerping = true;
+            newPosition = new Vector3(transform.position.x - 4, transform.position.y, transform.position.z);
+            StartCoroutine(LerpTowards(newPosition, 1));
         }
         // Jump forward
-        else if (Input.GetKeyUp(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W) && !isLerping)
         {
-            transform.Translate(Vector3.forward * moveSpeed);
+            isLerping = true;
+            newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 4);
+            platMove.MoveLastRow(newPosition);
+            StartCoroutine(LerpTowards(newPosition, 2));
+        }
+
+    }
+    // COROUTINES
+    public IEnumerator LerpTowards(Vector3 towardsPosition, int direction)
+    {
+        // Directions: 0 == LEFT, 1 == RIGHT, 2 == FORWARD, 3 == RIGHT FORWARD, 4 == LEFT FORWARD
+        while (isLerping)
+        {
+            //Debug.Log("Lerping like a fucker");
+            lerpTime += Time.deltaTime * rateOfLerp;
+            transform.position = Vector3.Lerp(transform.position, towardsPosition, lerpTime);
+            if ((transform.position.x >= towardsPosition.x - 0.05f && direction == 0) || 
+                (transform.position.x <= towardsPosition.x + 0.05f && direction == 1) ||  
+                (transform.position.z >= towardsPosition.z - 0.05f && direction == 2) || 
+                (transform.position.x <= towardsPosition.x + 0.05f && transform.position.z >= towardsPosition.z - 0.05f && direction == 3) ||
+                (transform.position.x >= towardsPosition.x - 0.05f && transform.position.z >= towardsPosition.z - 0.05f && direction == 4))
+            {
+                transform.position = towardsPosition;
+                isLerping = false;
+                lerpTime = 0;
+                StopCoroutine(LerpTowards(newPosition, direction));
+            }
+            yield return new WaitForSeconds(0);
         }
     }
 }
--- Assets/Scripts/PlatformsMovement.cs
+++ Assets/Scripts/PlatformsMovement.cs
@@ -1,33 +1,153 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlatformsMovement : MonoBehaviour {
     private float counter, timeToMeet, moveSpeed;
-    private bool playerMovedForward;
-	// Use this for initialization
-	void Start () {
+    private GameObject listOfPlatforms;
+    private GameObject[] platforms;
+    // START of LERP variables
+    bool isLerping, isLerpingLeft, isLerpingRight;
+    Vector3 newPosition, newPositionSideways;
+    float lerpTime, rateOfLerp, lerpTimeRight, lerpTimeLeft;
+    // END of LERP variables
+    // Platform generation variables
+    int randomPlatformNumber, unactivePlatformTracker, platformChosen;
+    bool isFirstPlatformActive;
+    // Use this for initialization
+    void Start () {
+        // Grab the platforms and assign them to the array of game objects, listOfPlatforms
+        listOfPlatforms = GameObject.FindGameObjectWithTag("platforms");
+        // Populate and initialize the array of platforms
+        platforms = new GameObject[15];
+        populatePlatforms();
         counter = 0; 
         // The time the player has to move to the next platform.
         timeToMeet = 100f;
-        // The boolean to check that the player is on the same
-        // platform row, otherwise the row moves forward
-        playerMovedForward = false;
         // Setting the move speed
         moveSpeed = 4.0f;
+        // Lerping variables
+        isLerping = false; isLerpingLeft = false; isLerpingRight = false;
+        lerpTime = 0.0f;
+        lerpTimeRight = 0.0f;
+        lerpTimeLeft = 0.0f;
+        rateOfLerp = 0.5f;
+        // Setting the positions for platform movement
+        newPosition = transform.position;
+        newPositionSideways = transform.position;
+        // Choosing platforms variables
+        randomPlatformNumber = -1;
+        unactivePlatformTracker = 0;
+        isFirstPlatformActive = false;
+        platformChosen = 0;
+        // GENERATE PLATFORMS
+        // Call function to start making the path
+        GeneratePlatforms();
     }
-	
 	// Update is called once per frame
 	void Update () {
-        MovePlatforms();
-        counter++;
-        Debug.Log(counter);
-	}
+        //MovePlatforms();
+        //counter++;
+        Debug.Log("Platform position: " + platforms[0].transform.position);
+    }
+    void FixedUpdate()
+    {
+        // Back up to make sure the lerping variables are reset.
+        if (transform.position == getNewPosition())
+        {
+            isLerpingRight = false; isLerpingLeft = false; isLerping = false;
+        }
+    }
+    // USER DEFINED FUNCTIONS
+    // Keeping variables indepedent from coroutines with a function to set counter
+    void setCounter(float newValue)
+    {
+        counter = newValue;
+    }
+    // Get the new position for use inside coroutines
+    Vector3 getNewPosition()
+    {
+        return new Vector3(newPositionSideways.x, newPositionSideways.y, newPosition.z);
+    }
+    void populatePlatforms()
+    {
+        for (int i =0; i < 15; i++)
+        {
+            platforms[i] = listOfPlatforms.transform.GetChild(i).gameObject;
+            platforms[i].gameObject.SetActive(false);
+        }
+    }
+    // Automatically move the platforms backwards after a certain time
     void MovePlatforms()
     {
-        if (counter >= timeToMeet && !playerMovedForward)
+        if (counter >= timeToMeet && !isLerping)
         {
-            transform.Translate(Vector3.back * moveSpeed);
-            counter = 0;
+            newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 4);
+            //StartCoroutine(LerpBackwards());
+            isLerping = true;
+        }
+    }
+    // Generating platforms, making them visible or invisible
+    void GeneratePlatforms()
+    {
+        // INDEPENDENT
+        if (!isFirstPlatformActive)
+        {
+            randomPlatformNumber = Mathf.RoundToInt(UnityEngine.Random.Range(0, 3));
+            platforms[randomPlatformNumber].gameObject.SetActive(true);
+            //Debug.Log("Holy Shit look at me::::: " + randomPlatformNumber);
+            GeneratePlatforms(randomPlatformNumber);
+            isFirstPlatformActive = true;
+        }
+    }
+    void GeneratePlatforms(int newPlatformNumber)
+    {
+        // PROBLEM: Less than 12 only works for the first round of gereration,
+        // Changes need for when old platforms must loop back around
+        if (newPlatformNumber <= 12)
+        {
+            if (platforms[newPlatformNumber].gameObject.tag == "left")
+            {
+                newPlatformNumber += Mathf.RoundToInt(UnityEngine.Random.Range(2, 4));
+                platforms[newPlatformNumber].gameObject.SetActive(true);
+                GeneratePlatforms(newPlatformNumber);
+            }
+            else if (platforms[newPlatformNumber].gameObject.tag == "right")
+            {
+                newPlatformNumber += Mathf.RoundToInt(UnityEngine.Random.Range(3, 5));
+                platforms[newPlatformNumber].gameObject.SetActive(true);
+                GeneratePlatforms(newPlatformNumber);
+            }
+            else if (platforms[newPlatformNumber].gameObject.tag == "center")
+            {
+                newPlatformNumber += Mathf.RoundToInt(UnityEngine.Random.Range(2, 5));
+                platforms[newPlatformNumber].gameObject.SetActive(true);
+                GeneratePlatforms(newPlatformNumber);
+            }
+        }
+    }
+    // !!------ PUBLIC FUNCTIONS ------!!
+    public void MovePlatformsOverwrite()
+    {
+        if (!isLerping && !isLerpingLeft && !isLerpingRight) {
+            newPosition = new Vector3(newPositionSideways.x, newPositionSideways.y, transform.position.z - 4);
+            //StartCoroutine(LerpBackwards());
+            isLerping = true;
+        }
+    }
+    // Move the 3 platforms behind the player to the front
+    public void MoveLastRow(Vector3 playerPosition)
+    {
+        Vector3 newRowPosition = playerPosition;
+        ArrayList listOfGameObjects = new ArrayList(platforms);
+        for(int i = 0; i < listOfGameObjects.Capacity; i++)
+        {
+            // LOOK UP HOW TO .GET IN AN ARRAYLIST
+            if (listOfGameObjects)
+            {
+                // Reset the position of the 3 gameobjects behind the player to be at the front
+                
+            }
         }
     }
 }
